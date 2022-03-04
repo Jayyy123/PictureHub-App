5dd41ef332c94e183c1139cfd883b546d7d69559
Jokeseph <josephofilii@gmail.com>
2022-03-04 23:21:35 +0100

    updated

--- WallpaperApp/app/src/main/java/com/jay/wallpaperapp/presentation/ui/activity/MainActivity.kt
+++ WallpaperApp/app/src/main/java/com/jay/wallpaperapp/presentation/ui/activity/MainActivity.kt
@@ -2,13 +2,14 @@ package com.jay.wallpaperapp.presentation.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.SimpleAdapter
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
-import androidx.viewbinding.ViewBinding
+
+import androidx.navigation.ui.AppBarConfiguration
+
 import com.jay.wallpaperapp.R
 import com.jay.wallpaperapp.databinding.ActivityMainBinding
 import com.jay.wallpaperapp.presentation.ui.fragments.FavouritesFragment
@@ -16,9 +17,14 @@ import com.jay.wallpaperapp.presentation.ui.fragments.ImageFragment
 import com.jay.wallpaperapp.presentation.ui.fragments.SearchFragment
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var  navontroller: NavController
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var  toggle:ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -27,35 +33,54 @@ class MainActivity : AppCompatActivity() {
         val favourites = FavouritesFragment()
         val images = ImageFragment()
 
-        supportActionBar?.hide()
-//        setfragment(images)
+//        supportActionBar?.hide()
 
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        setfragment(images)
 
-        navontroller = navHostFragment.navController
+        setSupportActionBar(binding.tb)
 
+//        val navHostFragment = supportFragmentManager
+//            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+//
+//        navontroller = navHostFragment.navController
+//        appBarConfiguration = AppBarConfiguration(setOf(R.id.imageFragment,R.id.favouritesFragment,R.id.searchFragment))
+//
+//        setupActionBarWithNavController(navontroller,appBarConfiguration)
 
-        binding.bnv.setupWithNavController(navontroller)
+//        binding.bnv.setupWithNavController(findNavController(R.id.fl))
 
 //        binding.bnv.setupWithNavController(binding.fragmentContainerView.findNavController())
 
-//        binding.bnv.setOnItemSelectedListener {
-//            when (it.itemId) {
-//                R.id.search_images_icon -> setfragment(search)
-//                R.id.images_icon -> setfragment(images)
-//                R.id.favourites_icon -> setfragment(favourites)
-//
-//            }
-//            true
-////
-////        }
-//        }
+        binding.bnv.setOnItemSelectedListener {
+
+            when (it.itemId) {
+                R.id.search_images_icon -> setfragment(search)
+                R.id.images_icon -> setfragment(images)
+                R.id.favourites_icon -> setfragment(favourites)
+            }
+            true
+        }
+        toggle = ActionBarDrawerToggle(this, binding.dl,R.string.open,R.string.close)
+        binding.dl.addDrawerListener(toggle)
+        toggle.syncState()
+
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
 
     }
 
-//    private fun setfragment(fragment: Fragment) = supportFragmentManager.beginTransaction().apply {
-//        replace(R.id.fragmentContainerView, fragment)
-//        commit()
-//    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+
+    private fun setfragment(fragment: Fragment) = supportFragmentManager.beginTransaction().apply {
+        replace(R.id.fragmentContainerView, fragment)
+        commit()
+    }
 }
